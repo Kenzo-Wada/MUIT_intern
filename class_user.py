80d1e6d3dde818fba39ffcfa5830d22d1913785b
SHO SOGAWA <sogawa.ritsumei@gmail.com>
2021-08-25 11:55:29 +0900

    予約情報を追加

--- class_user.py
+++ class_user.py
@@ -1,10 +1,11 @@
 # ユーザを表すのクラス
 class User():
-    def __init__(self, id, name, point, history):
+    def __init__(self, id, name, point, history, reserve):
         self.id = id # ユーザID（識別番号）
         self.name = name # 名前
         self.point = point # ポイント
-        self.history =history # チケット履歴
+        self.history = history # チケット履歴
+        self.reserve = reserve # 予約情報
 
     # ユーザID（識別番号）を返すメソッド
     def get_id(self):
@@ -21,3 +22,7 @@ class User():
     # チケット履歴を返すメソッド
     def get_history(self):
         return self.history
+
+    # 予約情報を返すメソッド
+    def get_reserve(self):
+        return self.reserve
