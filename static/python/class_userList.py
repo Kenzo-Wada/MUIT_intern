19e12a39f85ebbc82efe57bf120c1757f1ae0eae
masahiro-komatsu <bp19023@shibaura-it.ac.jp>
2021-08-25 19:31:34 +0900

    インスタンスを追加

--- static/python/class_userList.py
+++ static/python/class_userList.py
@@ -14,13 +14,9 @@ class UserList():
         user3 = class_user.User(3, "user3", 0, None, None)
         UserList.user_list += [user3]
     
+    # ユーザIDが一致するインスタンスを返すメソッド
     def search_user(self,key):
         for u in UserList.user_list:
             if u.get_id() == key:
-                   print(u.get_name())
+                   #print(u.get_name())
                    return u
-
-
-#for u in user_list:
-#   if u.get_id() == key_id:
-#       print(u.get_name())
