f72c185c86776233f17688250c793a8202d8034a
masahiro-komatsu <bp19023@shibaura-it.ac.jp>
2021-08-26 16:48:50 +0900

    ライブ情報をメインページに紐づけ

--- SystemController.py
+++ SystemController.py
@@ -1,3 +1,4 @@
+from entity.class_liveList import LiveList
 from os import truncate
 from flask import Flask, render_template, redirect, request
 
@@ -25,11 +26,15 @@ def check_login():
 # ライブ一覧ページ
 @app.route('/main')
 def show_main_page():
-    return render_template('main.html')
+    livelist = LiveList()
+    list = livelist.live_list
+    print(list)
+    return render_template('main.html',list_value = list)
 
 # ライブ詳細ページ
-@app.route('/detail')
-def show_detail_page():
+@app.route('/detail/<int:id>')
+def show_detail_page(id):
+    print(id)
     return render_template('detail.html')
 
 # 予約完了ページ
--- entity/class_liveList.py
+++ entity/class_liveList.py
@@ -1,5 +1,4 @@
-import class_live
-
+from entity import class_live
 #ライブリストを表すクラス
 class LiveList():
     # ライブリストの初期化
@@ -7,16 +6,19 @@ class LiveList():
 
     def __init__(self):
         # ライブの作成とリストへの追加
-        live1 = class_live.Live(1, "Bunp of Chicken", 7000, "横浜アリーナ(神奈川)", "17:00", "20:00", "15:30", "2021/9/20 (月)", None)
-        LiveList.live_list += [live1]
-        live2 = class_live.Live(2, "ONE OK ROCK", 7000, "さいたまスーパーアリーナ(埼玉)", "13:00 - 16:00", None)
-        LiveList.live_list += [live2]
-        live3 = class_live.Live(3, "RADWIMPS", 9000, "東京ドーム(東京)", "13:00 - 15:00", None)
-        LiveList.live_list += [live3]
-    
+        live1 = class_live.Live(1, "Bunp of Chicken", "¥7,000", "横浜アリーナ(神奈川)", "17:00", "20:00", "15:30", "2021/9/20 (月)", None)
+        self.live_list.append(live1)
+        live2 = class_live.Live(2, "ONE OK ROCK", "¥5,000", "さいたまスーパーアリーナ(埼玉)", "13:00", "16:00", "12:00", "2021/9/26 (日)",  None)
+        self.live_list.append(live2)
+        live3 = class_live.Live(3, "RADWIMPS", "¥9,000", "東京ドーム(東京)", "13:00", "15:00", "11:30", "2021/9/18 (土)", None)
+        self.live_list.append(live3)
+        live4 = class_live.Live(4, "RADWIMPS", "¥9,000", "東京ドーム(東京)", "15:00", "17:00", "13:30", "2021/9/19 (日)", None)
+        self.live_list.append(live4)
+        live5 = class_live.Live(5, "King Gnu", "¥7,000", "東京ドーム(東京)", "13:00", "16:00", "11:30", "2021/9/26 (日)", None)
+        self.live_list.append(live5)
     # ライブIDが一致するインスタンスを返すメソッド
     def search_live(self,key):
-        for l in LiveList.live_list:
+        for l in self.live_list:
             if l.get_id() == key:
                    #print(u.get_name())
                    return l
